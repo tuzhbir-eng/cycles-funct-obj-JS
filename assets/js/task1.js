eb3249a7a75dd9598a3029725aa8a5a3eaacdd83
vvvgit config --global user.name vvvgit config --global user.name vvv <tuzhbir@gmail.com>
2026-07-31 22:27:31 +0300

    task1.2

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -1,17 +1,68 @@
+console.group("1. ✅ Вивести числа від 25 до 0 (порядок зменшення).");
+
 console.group("=====While=====");
-let num = 26;
-while(num>0) {
-    num--
-    console.log(num)
+let numDecrease = 26;
+while (numDecrease > 0) {
+  numDecrease--;
+  console.log(numDecrease);
 }
 console.groupEnd();
 
 
 
 
+
+
 console.group("=====For=====");
-for (let num2 = 25; num2 >= 0; num2--) {
-  console.log(num2);
+for (let numDecrease2 = 25; numDecrease2 >= 0; numDecrease2--) {
+  console.log(numDecrease2);
+}
+console.groupEnd();
+
+
+
+
+
+
+console.group("=====Do-while=====");
+let numDecrease3 = 25;
+do {
+  console.log(numDecrease3);
+  numDecrease3--;
+} while (numDecrease3 >= 0);
+console.groupEnd();
+console.groupEnd();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.group("2. ✅ Вивести числа від 10 до 50, які кратні 5.")
+
+console.group("=====While=====")
+let numMultiple = 5;
+while (numMultiple <50) {
+    numMultiple+=5;
+    console.log(numMultiple);
+}
+console.groupEnd();
+
+
+
+console.group("=====For=====");
+
+for(let numMultiple2 = 10; numMultiple2<=50; numMultiple2+=5) {
+    console.log(numMultiple2)
 }
 console.groupEnd();
 
@@ -19,9 +70,24 @@ console.groupEnd();
 
 
 console.group("=====Do-while=====");
-let num3 = 25;
+let numMultiple3 = 10;
 do {
-    console.log(num3);
-    num3--;
-} while (num3>=0)
+    console.log(numMultiple3)
+    numMultiple3+=5
+} while(numMultiple3<=50)
+
 console.groupEnd();
+console.groupEnd();
+
+
+
+
+
+
+
+
+
+
+
+
+
