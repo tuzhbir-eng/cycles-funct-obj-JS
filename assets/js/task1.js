09c708d0ce4c646001a9198eb94dcb6fa1812561
vvvgit config --global user.name vvvgit config --global user.name vvv <tuzhbir@gmail.com>
2026-07-31 21:59:02 +0300

    for

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -1,5 +1,21 @@
+console.group("=====While=====");
 let num = 26;
 while(num>0) {
     num--
     console.log(num)
-} 
+}
+console.groupEnd();
+
+
+
+
+console.group("=====For=====");
+for (let num2 = 25; num2 >= 0; num2--) {
+  console.log(num2);
+}
+console.groupEnd();
+
+
+
+
+
