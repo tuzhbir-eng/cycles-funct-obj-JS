b58d9ba1412ff1d6ab85679fcaa3470fdd1e10c7
vvvgit config --global user.name vvvgit config --global user.name vvv <tuzhbir@gmail.com>
2026-08-02 22:48:19 +0300

    calculateSurfaceArea

--- assets/js/function.js
+++ assets/js/function.js
@@ -21,3 +21,17 @@ console.log(checkMultiplicity(15, 4)) // false
 console.groupEnd();
 
 /////////////////////////////////////////////////////////////////////////////////
+
+function calculateSurfaceArea(shapeType, param1, param2=0) {
+if(shapeType==="cube") {
+    return 6*param1**2;
+} if(shapeType==="cylinder") {
+    return (2*Math.PI*param1*(param1+param2));
+}
+}
+console.group('Area');
+console.log(calculateSurfaceArea("cube", 4));
+console.log(calculateSurfaceArea("cylinder", 5, 8));
+console.groupEnd();
+
+///////////////////////////////////////////////////////////////////////////////////
