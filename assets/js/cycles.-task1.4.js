95522b9c36171f8df70075a9f85e0964beff1253
vvvgit config --global user.name vvvgit config --global user.name vvv <tuzhbir@gmail.com>
2026-08-03 00:28:20 +0300

    some changes

--- assets/js/cycles.-task1.4.js
+++ assets/js/cycles.-task1.4.js
@@ -1,5 +1,6 @@
 let tries = Number(prompt('Type your number: '));
 const num = (Math.floor(Math.random() * 20) + 1);
+
 while (tries <= 20) {
   if(tries !==num) {
     tries = Number(prompt("Try again: "));
