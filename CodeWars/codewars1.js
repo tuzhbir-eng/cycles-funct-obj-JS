b6d9817bbef1f22c770361fc256b4226179ddb30
vvvgit config --global user.name vvvgit config --global user.name vvv <tuzhbir@gmail.com>
2026-08-08 14:10:20 +0300

    some changes at link2

--- CodeWars/codewars1.js
+++ CodeWars/codewars1.js
@@ -1,30 +1,23 @@
 var calculate = function calculate(a, o, b) {
- var result = 0;
-  if(o!=="+" || o!=="-" || o!=="*" || o!=="/") {
-  
- if(o === "+") {    
-   return a + b;
-  } 
- if(o === "-") {
-   return a - b;
+  var result = null;
+  if (o === "+") {
+    return a + b;
   }
- if(o === "/") { 
- if(b===0) {
-   return null;
- }
-   return a / b;
- }
- if(o === "*") { 
-   return a * b;
- }   
- return null;
+  if (o === "-") {
+    return a - b;
   }
-return result; 
-}
+  if (o === "/" && b !== 0) {
+    return a / b;
+  }
+  if (o === "*") {
+    return a * b;
+  }
+  return result;
+};
 
-    console.log(calculate(2, "+", 4)); //Should return 6
-    console.log(calculate(6, "-", 1.5)); //Should return 4.5
-    console.log(calculate(-4, "*", 8)); //Should return -32
-    console.log(calculate(49, "/", -7)); //Should return -7
-    console.log(calculate(8, "m", 2)); //Should return null
-    console.log(calculate(4, "/", 0)); //should return null
+console.log(calculate(2, "+", 4)); //Should return 6
+console.log(calculate(6, "-", 1.5)); //Should return 4.5
+console.log(calculate(-4, "*", 8)); //Should return -32
+console.log(calculate(49, "/", -7)); //Should return -7
+console.log(calculate(8, "m", 2)); //Should return null
+console.log(calculate(4, "/", 0)); //should return null
